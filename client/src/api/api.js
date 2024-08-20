3b3a8b4edd68cee8ce42555bd1169e1af1a065e6
SeifeddineMechergui <saifmechergui2222@gmail.com>
2024-08-20 17:11:46 +0100

    fix bugs

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const local = 'http://localhost:5000'
-const production = 'https://shiningtn.onrender.com'
+const production = 'https://shining-api.onrender.com'
 
 let api_url=''
 let mode='pro'
